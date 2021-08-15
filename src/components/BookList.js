d10b1dd91b3bc01c1779f52d55476d0040c46b05
Ngoc Tu <tdngoctu@gmail.com>
2021-08-15 23:57:38 +0700

    use consumer for contextAPI

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -5,17 +5,23 @@ class BookList extends Component {
     static contextType = ThemeContext
 
     render() { 
-        const {isLightTheme, light, dark} = this.context
-        const theme = isLightTheme? light: dark
-        return ( 
-            <div className='book-list' style={{background: theme.bg}}>
-                <ul>
-                    <li style={{background: theme.ui}}>the way of kings</li>
-                    <li style={{background: theme.ui}}>the name of wind</li>
-                    <li style={{background: theme.ui}}>the final empire</li>
-                </ul>
-            </div>
-         );
+        return (
+            <ThemeContext.Consumer>{(context) =>{
+                const {isLightTheme, light, dark} = context
+                const theme = isLightTheme? light: dark
+                return ( 
+                    <div className='book-list' style={{background: theme.bg}}>
+                        <ul>
+                            <li style={{background: theme.ui}}>the way of kings</li>
+                            <li style={{background: theme.ui}}>the name of wind</li>
+                            <li style={{background: theme.ui}}>the final empire</li>
+                        </ul>
+                    </div>
+                );
+                }}
+            </ThemeContext.Consumer>
+        )
+
     }
 }
  
