b251241b793d19eff4f5474cc4008fa6bffc5f61
Ngoc Tu <tdngoctu@gmail.com>
2021-08-16 11:38:53 +0700

    function context on booklist

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,26 +1,43 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-    render() { 
-        return (
-            <ThemeContext.Consumer>{(context) =>{
-                const {isLightTheme, light, dark} = context
-                const theme = isLightTheme? light: dark
-                return ( 
-                    <div className='book-list' style={{background: theme.bg}}>
-                        <ul>
-                            <li style={{background: theme.ui}}>the way of kings</li>
-                            <li style={{background: theme.ui}}>the name of wind</li>
-                            <li style={{background: theme.ui}}>the final empire</li>
-                        </ul>
-                    </div>
-                );
-                }}
-            </ThemeContext.Consumer>
-        )
+// class BookList extends Component {
+//     render() { 
+//         return (
+//             <ThemeContext.Consumer>{(context) =>{
+//                 const {isLightTheme, light, dark} = context
+//                 const theme = isLightTheme? light: dark
+//                 return ( 
+//                     <div className='book-list' style={{background: theme.bg}}>
+//                         <ul>
+//                             <li style={{background: theme.ui}}>the way of kings</li>
+//                             <li style={{background: theme.ui}}>the name of wind</li>
+//                             <li style={{background: theme.ui}}>the final empire</li>
+//                         </ul>
+//                     </div>
+//                 );
+//                 }}
+//             </ThemeContext.Consumer>
+//         )
 
-    }
-}
+//     }
+// }
  
+// export default BookList;
+
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext)
+    const theme = isLightTheme? light: dark
+
+    return (
+        <div className='book-list' style={{background: theme.bg}}>
+            <ul>
+                <li style={{background: theme.ui}}>the way of kings</li>
+                <li style={{background: theme.ui}}>the name of wind</li>
+                <li style={{background: theme.ui}}>the final empire</li>
+            </ul>
+        </div>
+    )
+}
+
 export default BookList;
